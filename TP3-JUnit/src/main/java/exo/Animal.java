82a5d33afdd179d23d60c4a210d86939d4df15b9
guillaume <guillaume.chanson@dbmail.com>
2014-12-10 16:09:09 +0100

    Animal - TP3

---
+++ TP3-JUnit/src/main/java/exo/Animal.java
@@ -0,0 +1,138 @@
+package exo;
+
+/**
+ * Classe Animal qui caractérise un animal.
+ * 
+ * @author Guillaume Chanson - François Chalifour
+ * @since 10/12/14
+ * @version 1.0
+ */
+public class Animal {
+
+	private String espece;
+	private boolean carnivore;
+	private float poids;
+
+	/**
+	 * Crée un nouvel animal à partir des informations données.
+	 * 
+	 * @param espece
+	 *            l'espèce de l'animal
+	 * @param carnivore
+	 *            le régime alimentaire de l'animal : carnivore (true) ou non
+	 *            (false)
+	 * @param poids
+	 *            le poids de la bête (kg)
+	 */
+	public Animal(String espece, boolean carnivore, float poids) {
+		this.espece = espece;
+		this.carnivore = carnivore;
+		this.poids = poids;
+	}
+
+	/**
+	 * Retourne l'espèce de l'animal.
+	 * 
+	 * @return l'espèce
+	 */
+	public String getEspece() {
+		return espece;
+	}
+
+	/**
+	 * Modifie l'espèce de l'animal.
+	 * 
+	 * @param espece
+	 *            la nouvelle espèce
+	 */
+	public void setEspece(String espece) {
+		this.espece = espece;
+	}
+
+	/**
+	 * Indique si l'animal est carnivore.
+	 * 
+	 * @return <code>true</code> si l'animal est carnivore; <br>
+	 *         <code>false</code> sinon
+	 */
+	public boolean isCarnivore() {
+		return carnivore;
+	}
+
+	/**
+	 * Modifie le régime alimentaire de l'animal.
+	 * 
+	 * @param carnivore
+	 *            le nouveau régime alimentaire : carnivore (true) ou non
+	 *            (false)
+	 */
+	public void setCarnivore(boolean carnivore) {
+		this.carnivore = carnivore;
+	}
+
+	/**
+	 * Retourne le poids de l'animal.
+	 * 
+	 * @return le poids (kg)
+	 */
+	public float getPoids() {
+		return poids;
+	}
+
+	/**
+	 * Modifie le poids de l'animal.
+	 * 
+	 * @param poids
+	 *            le nouveau poids de l'animal (kg)
+	 */
+	public void setPoids(float poids) {
+		this.poids = poids;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Animal [espece=" + espece + ", carnivore=" + carnivore
+				+ ", poids=" + poids + "]";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (carnivore ? 1231 : 1237);
+		result = prime * result + ((espece == null) ? 0 : espece.hashCode());
+		result = prime * result + Float.floatToIntBits(poids);
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj instanceof Animal) {
+			final Animal animal = (Animal) obj;
+			if (animal.getEspece() == espece && animal.getPoids() == poids
+					&& animal.isCarnivore() == carnivore)
+				return true;
+		}
+		
+		return false;
+	}
+
+}
---
+++ TP3-JUnit/src/test/java/exo/AnimalTest.java
@@ -0,0 +1,77 @@
+package exo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Classe de test de la classe Animal
+ * 
+ * @author Guillaume Chanson - François Chalifour
+ * @since 10/12/14
+ * @version 1.0
+ */
+public class AnimalTest {
+
+	@Test
+	public void testSingleAnimalToString() {
+		// Given
+		final String espece = "Lion";
+		final boolean carnivore = true;
+		final float poids = 217.5F;
+
+		final Animal animal = new Animal(espece, carnivore, poids);
+
+		// Then
+		final String expected = "Animal [espece=" + espece + ", carnivore="
+				+ carnivore + ", poids=" + poids + "]";
+
+		assertEquals(expected, animal.toString());
+	}
+
+	@Test
+	public void testEqualsTrueMemeAnimal() {
+		// Given
+		final Animal animal = new Animal("Lion", true, 217.5F);
+
+		// Then
+		assertTrue(animal.equals(animal));
+	}
+	
+	@Test
+	public void testEqualsTrueDeuxAnimauxIdentiques() {
+		// Given
+		final String espece = "Lion";
+		final boolean carnivore = true;
+		final float poids = 217.5F;
+
+		final Animal animal1 = new Animal(espece, carnivore, poids);
+		final Animal animal2 = new Animal(espece, carnivore, poids);
+
+		// Then
+		assertTrue(animal1.equals(animal2));
+	}
+
+	@Test
+	public void testEqualsFalseDeuxAnimauxDifferents() {
+		// Given
+		final Animal animal1 = new Animal("Lion", true, 217.5F);
+		final Animal animal2 = new Animal("Tortue Sous-Marine", false, 12.3F);
+
+		// Then
+		assertFalse(animal1.equals(animal2));
+	}
+
+	@Test
+	public void testEqualsFalseAnimalEtNonAnimal() {
+		// Given
+		final Animal animal = new Animal("Perruche", false, 2.5F);
+		final Object obj = new Object();
+		
+		// Then
+		assertFalse(animal.equals(obj));
+	}
+
+}
