13edfcb4d54b7d11d94755cc69da830e9354a250
guillaume <guillaume.chanson@dbmail.com>
2014-12-17 19:31:21 +0100

    TP4 : manque la fixture à faire

--- TP4-Mockito/src/main/java/authentification/Compte.java
+++ TP4-Mockito/src/main/java/authentification/Compte.java
@@ -1,93 +1,116 @@
 package authentification;
 
 /**
- * Compte utilisateur permettant d'acceder au systeme
- *
+ * Compte utilisateur permettant d'acceder au système.
+ * 
  */
 public class Compte {
 
 	/**
-	 * Identifiant du compte utilisateur
+	 * Identifiant du compte utilisateur.
 	 */
 	protected String identifiant;
 
 	/**
-	 * Le compte est actif (ou non)
+	 * Le compte est actif (ou non).
 	 */
 	protected boolean actif;
 
 	/**
-	 * Constructeur avec arguments
+	 * Créé un Compte avec un identifiant et un mot de passe donné.
+	 * 
 	 * @param identifiant
+	 *            l'identifiant du compte
 	 * @param motDePasse
+	 *            le mot de passe du compte
 	 */
 	public Compte(String identifiant) {
 		this.identifiant = identifiant;
 		this.actif = true;
 	}
 
+	/**
+	 * Retourne l'identifiant du compte.
+	 * 
+	 * @return l'identifiant
+	 */
 	public String getIdentifiant() {
 		return identifiant;
 	}
 
+	/**
+	 * Modifie l'identifiant du compte.
+	 * 
+	 * @param identifiant
+	 *            l'identifant du compte
+	 */
 	public void setIdentifiant(String identifiant) {
 		this.identifiant = identifiant;
 	}
 
+	/**
+	 * Retourne un booléen spécifiant l'activité du compte.
+	 * 
+	 * @return <code>true</code> si le compte est actif; <br>
+	 *         <code>false<code> sinon
+	 */
 	public boolean isActif() {
 		return actif;
 	}
 
+	/**
+	 * Place le compte à "actif" (true), ou "inactif" (false).
+	 * 
+	 * @param actif
+	 *            le booléen spécifiant l'activité du compte
+	 */
 	public void setActif(boolean actif) {
 		this.actif = actif;
 	}
 
-	/**
-	 * Impl�mentation du hashCode
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @return la valeur du hashCode de l'objet
+	 * @see java.lang.Object#hashCode()
 	 */
+	@Override
 	public int hashCode() {
-		// On choisit deux nombres impairs
-		final int multiplieur = 17;
-		int result = 7;
+		final int prime = 31;
+		int result = 1;
 
-		// Pour chaque attribut, on calcule le hashcode
-	    // que l'on ajoute au resultat
-		// apres l'avoir multipli� par le nombre "multiplieur"
-		result = (multiplieur * result) + identifiant.hashCode();
+		result = prime * result + (actif ? 1231 : 1237);
+		result = prime * result
+				+ ((identifiant == null) ? 0 : identifiant.hashCode());
+
 		return result;
 	}
 
-	/**
-	 * Impl�mentation du equals : on va comparer les identifiants de chaque compte
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @param obj l'objet � comparer avec l'instance courante pour d�terminer s'ils sont �gaux
-	 * 
-	 * @return true si les deux objets sont �gaux, false sinon
+	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Compte other = (Compte) obj;
-		if (identifiant != other.identifiant)
-			return false;
-		return true;
+
+		if (obj instanceof Compte) {
+			if (identifiant == ((Compte) obj).identifiant)
+				return true;
+		}
+
+		return false;
 	}
 
-	/**
-	 * Surcharge de la methode toString pour personnaliser l'affichage
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
 	 */
+	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("id=[").append(this.identifiant)
-		  .append("],actif=[").append(this.actif).append("]");
-		return sb.toString();
+		return "Compte [identifiant=" + identifiant + ", actif=" + actif + "]";
 	}
 
 }
